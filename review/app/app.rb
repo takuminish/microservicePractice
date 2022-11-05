4c22c9a8f9a81dff390ed364f6bf6073081d44f2
takuminish <takuminv1997@gmail.com>
2022-11-05 21:25:09 +0900

    Sinatraからmysqlに接続しreviewデータを取得

--- review/app/app.rb
+++ review/app/app.rb
@@ -1,8 +1,16 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'active_record'
+require 'mysql2'
 
 class ReviewApplication < Sinatra::Base
     get '/' do
-    "Hello World"
+    config  = YAML.safe_load(File.open("./database.yml", "r"))
+
+    ActiveRecord::Base.establish_connection(config['development'])
+   
+    response = {reviews: ActiveRecord::Base.connection.select_all("select * from review")}
+
+    return response.to_json
     end
 end
